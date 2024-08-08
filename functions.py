673adfbd064706deec6da7b806850f6edef0254a
sumeyyeklc <sumeyyekilic184@gmail.com>
2024-08-08 23:26:01 +0300

    Functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+def greeting(name):
+    print("Hello" + name)
+greeting("everybody")
+
+def app_calculation(number):
+    total = sum(number)
+    count = len(number)
+    app = total / count
+    print(f"app : {app}")
+
+app_calculation([1,2,3,4,5])
+
+def upper_Letter(text):
+    return text.upper()
+text = input("Write something: ")
+print(upper_Letter(text))
